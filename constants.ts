bf7154692fcba61489753dfcd82c868609a59555
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2024-12-09 18:04:09 +0100

    assets exports

---
+++ constants.ts
@@ -0,0 +1,11 @@
+export const BANNER_IMAGES = [
+   '/banners/padthai.png',
+   '/banners/burgers.png',
+   '/banners/icecream.png',
+   '/banners/mochi.png',
+   '/banners/hybrid.png',
+   '/banners/smoothie.png',
+   '/banners/bubbletea2.png',
+   '/banners/mangorice.png',
+   '/banners/bubbletea1.png',
+ ]; 
