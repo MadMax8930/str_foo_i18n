93455ad4e2b4c89ac82ef95fd294c44358189240
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2024-12-09 18:39:15 +0100

    v1.2 done

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -4,10 +4,10 @@ import { BANNER_IMAGES } from '../../constants';
 const Gallery = () => {
   return (
     <section className="w-full px-4 sm:px-8 lg:px-16 py-6 sm:py-12 bg-gray-100">
-      <h2 className="text-center text-2xl sm:text-3xl font-extrabold text-gray-700 mb-8">
+      <h2 className="text-center text-2xl sm:text-3xl font-extrabold text-gray-700 mb-8 pt-4">
         My Banner Designs
       </h2>
-      <div className="space-y-4">
+      <div className="space-y-2 sm:space-y-4 md:space-y-6 opacity-90">
         {BANNER_IMAGES.map((banner, index) => (
           <div key={index} className="relative w-full h-16 sm:h-24 lg:h-32">
             <Image
@@ -15,7 +15,7 @@ const Gallery = () => {
               alt={`Banner ${index + 1}`}
               layout="fill"
               objectFit="cover"
-              className="rounded-lg shadow-md"
+              className="rounded-lg shadow-md border-[3px] border-primary"
             />
           </div>
         ))}
